8f91b62905d2d4e74633e1abdf849ab3b2409e89
FelipeHansen <felipehansen4@gmail.com>
2024-10-31 11:11:59 -0300

    Separando o grafo em mais de 2 subGrupos

--- main.c
+++ main.c
@@ -6,8 +6,7 @@
     float matrizEuclidiana[150][150];
     float matrizNormal[150][150];
     int matrizAdjacente[150][150];
-    int grupoA[150];
-    int grupoB[150];
+    int subGrupos[150][150];
 
 int main(int argc, char *argv[]){
     int opcao;
@@ -19,22 +18,35 @@ int main(int argc, char *argv[]){
             vet = lerCSV(argv[1]);
             distanciaEuc(vet, matrizEuclidiana);
             normalizaMatriz(matrizEuclidiana, matrizNormal);
-            matrizAdj(matrizNormal, 0.2, matrizAdjacente);
+            matrizAdj(matrizNormal, 0.05, matrizAdjacente);
             gravarCSV(matrizEuclidiana, matrizNormal, matrizAdjacente);
         break;
         case 1:
             lerPersistencia(matrizAdjacente, "grafo.csv");
         break;
     }
-    
-    DFS(matrizAdjacente, 0, grupoA);
+    DFS(matrizAdjacente, 0, subGrupos[0]);
+    int qtdSubGrpos = 1;
+    int vdd = 1;
     for (int i = 0; i < 150; i++){
-        if (grupoA[i] == 0){
-            DFS(matrizAdjacente, i, grupoB);
+        if (subGrupos[qtdSubGrpos-1][i] == 0){
+            for (int j = 0; j < 150; j++){
+                if (subGrupos[j][i] != 0){
+                    vdd = 0;
+                    break;
+                }
+            } 
+            if (vdd){
+                qtdSubGrpos++;
+                 DFS(matrizAdjacente, i, subGrupos[qtdSubGrpos-1]);
+            }
         }
-        
     }
-    printf("Acuracia: %lf\n", acuracia(grupoA, grupoB, vet));
+    
+
+
+    printf("Quantidade de subGrafos: %i\n", qtdSubGrpos);
+    printf("Acuracia: %lf\n", acuracia(subGrupos[0], subGrupos[1], vet));
     // printGrupos(grupoA);
     // printf("-------------------------------\n\n");
     // printGrupos(grupoB);
